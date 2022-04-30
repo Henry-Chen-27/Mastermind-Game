fd2403ad7f56e07d6062ba4a3c1341edbc39e803
Henry-Chen-27 <81109252+Henry-Chen-27@users.noreply.github.com>
2022-04-30 11:41:21 -0400

    Add files via upload

---
+++ mastermind.py
@@ -0,0 +1,14 @@
+# Mastermind game
+import random
+
+# TODO: Generate Solution
+
+
+# TODO: Implement checking methods
+
+
+# TODO: Implement user solution input
+
+
+# TODO: OPTIONAL, Implement settings
+# TODO: Play game
